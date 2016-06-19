56d2fa3c55d22065984277aba7aabcc9eb366b79
adbooth <adbooth8@gmail.com>
2016-06-19 12:58:34 -0400

    Some streamlining, filename changes and template changes

--- app.py
+++ app.py
@@ -12,55 +12,26 @@ from werkzeug.utils import secure_filename
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
 
-def allowed_type(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
-
-def index_add(filename, title, blurb):
-    with open(app.config['INDEX_FILE'], 'r+') as indexFile:
-        index = json.load(indexFile)
-        index.append({
-            'timestamp': timegm(gmtime()),
-            'filename': filename,
-            'title': title,
-            'blurb': blurb
-        })
-
-        indexFile.seek(0)
-        indexFile.truncate()
-        json.dump(index, indexFile)
-
-
 # Set up routes
 @app.route('/')
-def root():
+def home():
+    """ Build page with image data file sorted by timestamp """
     with open(app.config['INDEX_FILE'], 'r') as indexFile:
         index = json.load(indexFile)
     index.sort(key=lambda d: d['timestamp'], reverse=True)
-    return render_template('root.html', index=index)
-
-
-@app.route('/photo/<filename>')
-def photo(filename):
-    with open(app.config['INDEX_FILE'], 'r') as indexFile:
-        index = json.load(indexFile)
-
-    for image in index:
-        if image['filename'] == filename: break
-
-    return render_template('photo.html', image=image)
-
+    return render_template('home.html', index=index)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    """ Show login and check password against password environment var """
     error = ''
-    if 'message' in session:
-        error = session['message']
-
     if request.method == 'POST':
         if request.form['password'] == app.config['PASSWORD']:
+            # Correct password, set session var and redirect
             session['loggedIn'] = True
             return redirect('/upload')
         else:
+            # Incorrect pasword, ask to try again
             session['loggedIn'] = False
             error = 'Incorrect password'
 
@@ -69,24 +40,40 @@ def login():
 
 @app.route('/upload', methods=['GET', 'POST'])
 def upload():
+    """ Send upload page if logged in """
+
     if 'loggedIn' not in session or not session['loggedIn']:
-        session['message'] = 'Not logged in'
+        # Not logged in, send to login page
         return redirect('/login')
 
     error = ''
     if request.method == 'POST':
-        if 'file' not in request.files:
-            error = 'No file part'
-
         image = request.files['file']
         filename = image.filename
-        if filename == '':
-            error = 'No selected image'
 
-        if not error and image and allowed_type(filename):
+        # Make sure file exists and is of the right type
+        if filename == '':
+            error = 'No file selected'
+        elif not ('.' in filename and filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']):
+            error = 'Bad image filename'
+
+        if not error and image:
+            # Save file
             filename = secure_filename(filename)
             image.save(path.join(app.config['UPLOAD_FOLDER'], filename))
-            index_add(filename, request.form['title'], request.form['blurb'])
+            # Add file to index
+            with open(app.config['INDEX_FILE'], 'r+') as indexFile:
+                index = json.load(indexFile)
+                index.append({
+                    'timestamp': timegm(gmtime()),
+                    'filename': filename,
+                    'title': request.form['title'],
+                    'blurb': request.form['blurb']
+                })
+                indexFile.seek(0)
+                indexFile.truncate()
+                json.dump(index, indexFile)
+            # Redirect to home page
             return redirect('/')
 
     return render_template('upload.html', error=error)
