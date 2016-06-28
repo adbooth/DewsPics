be7b695d558ddd052206dd18413c1ba4bc5ea450
adbooth <adbooth8@gmail.com>
2016-06-28 18:11:53 -0400

    DB update

--- config.py
+++ config.py
@@ -5,10 +5,10 @@ from os import environ, urandom
 # From environment
 DEBUG = environ['DEBUG']
 PYTHONUNBUFFERED = environ['PYTHONUNBUFFERED']
+MONGO_URI = environ['MONGODB_URI']
 PASSWORD = environ['PASSWORD']
 if 'SECRET_KEY' not in environ:
     SECRET_KEY = urandom(24)
-
 
 # Constants
 UPLOAD_FOLDER = 'static/img'
